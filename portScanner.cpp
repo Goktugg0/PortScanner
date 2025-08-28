fec22f9529443c8a66ddab925935932fba5a2667
Çakmaklı <g.cakmakli@student.tue.nl>
2025-08-28 05:42:42 +0200

    testing user inputs

--- portScanner.cpp
+++ portScanner.cpp
@@ -6,4 +6,9 @@ int main() {
     for (int i = 0; i < 5; i++) {
     cout << i << "\n";
     }
+
+    int x; 
+    cout << "Type a number: "; // Type a number and press enter
+    cin >> x; // Get user input from the keyboard
+    cout << "Your number is: " << x; // Display the input value
 }
