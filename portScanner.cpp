dfd07688e968b6c1b3e9e8e6d4f67949c285b912
Çakmaklı <g.cakmakli@student.tue.nl>
2025-09-09 12:00:33 +0200

    I was coding for so long on my projects but they don't show my commits but now they should be

--- portScanner.cpp
+++ portScanner.cpp
@@ -3,11 +3,12 @@ using namespace std;
 
 void scanPort(std::string ip, int port) {
     cout << "Port " << port << " is OPEN \n";
+    // Actual port scanning
 }
 
 
 int main() {
-    
+    """Test commits for my account because it was not working """;
     std::string IP = "192.168.1.1"; // Example IP
     int startPort = 1;
     int endPort = 65536;
