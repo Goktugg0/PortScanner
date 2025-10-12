1329e8642f9b19f5027ed5a9752b3e43153ce986
Goktugg0 <goktugcakmakli35@gmail.com>
2025-10-13 01:50:56 +0200

    dealing with while loop wil look at the scanning part

--- portScannerWindows.cpp
+++ portScannerWindows.cpp
@@ -1,20 +1,38 @@
 #include <iostream>
 using namespace std;
 
-void scanPort(std::string ip, int port) {
+bool scanPort(std::string ip, int port) {
     cout << "Port " << port << " is OPEN \n";
     // Actual port scanning
 }
 
 
-int main() {
-    """Test commits one more time and yes it is working""";
-    std::string IP = "127.0.0.1"; // Example IP
-    int startPort = 1;
-    int endPort = 65536;
+int main(int argc, char* argv[]) {
+
+    char const* IP = "127.0.0.1"; // Local IP for some reasons
     
+    // Default values
+    int startPort = 1;
+    int endPort = 1024;
+    int timeOut = 200; // ms
+
+    if (argc >= 2) {
+        startPort = atoi(argv[1]);
+    }
+    if (argc >= 3) {
+        endPort = atoi(argv[2]);
+    }
+    if (argc >= 4) {
+        timeOut = atoi(argv[3]);
+    }
+
+    std:: cout << "Starting scanning on " << IP << "from port number" <<
+        startPort << "to" << endPort << "with timeout " << timeOut;
+
     for (int currPort = startPort; currPort < endPort; currPort++) {
-        scanPort(IP, currPort);
+        if (scanPort(IP, currPort)) {
+            std:: cout << "Port number " << currPort << "is open \n";
+        }
     }
 
 }
