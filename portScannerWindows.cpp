d3406e76f0eca213543a4924bbbd4141d7a38da6
Goktugg0 <goktugcakmakli35@gmail.com>
2025-10-13 02:14:36 +0200

    main function is probably ready(?)

--- portScannerWindows.cpp
+++ portScannerWindows.cpp
@@ -1,29 +1,43 @@
 #include <iostream>
 using namespace std;
 
+#pragma comment(lib,"ws2_32.lib") // winsock for Windows
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
+
 bool scanPort(std::string ip, int port) {
     cout << "Port " << port << " is OPEN \n";
     // Actual port scanning
 }
 
-
 int main(int argc, char* argv[]) {
 
-    char const* IP = "127.0.0.1"; // Local IP for some reasons
-    
+    char const* IP = "127.0.0.1"; // Local IP for reasons
     // Default values
     int startPort = 1;
     int endPort = 1024;
     int timeOut = 200; // ms
 
     if (argc >= 2) {
-        startPort = atoi(argv[1]);
+        IP = argv[1];
     }
     if (argc >= 3) {
-        endPort = atoi(argv[2]);
+        startPort = atoi(argv[2]);
     }
     if (argc >= 4) {
-        timeOut = atoi(argv[3]);
+        endPort = atoi(argv[3]);
+    }
+    if (argc >= 5) {
+        timeOut = atoi(argv[4]);
+    }
+
+    // Initialize Winsock
+    WSADATA wsaData;
+    int isResult = WSAStartup(MAKEWORD(2,2), &wsaData);
+    if (isResult != 0) {
+        std:: cerr << "WSAStartup failed: %d\n", isResult;
+        return 1;
     }
 
     std:: cout << "Starting scanning on " << IP << "from port number" <<
@@ -34,5 +48,9 @@ int main(int argc, char* argv[]) {
             std:: cout << "Port number " << currPort << "is open \n";
         }
     }
+
+    std::cout << "Done \n";
+
+    return 0;
 
 }
